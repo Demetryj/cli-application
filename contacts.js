bae084e0e70990600d5bb0ccac5320e70d4cd3fb
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-01-27 20:34:41 +0300

    Create contacts.js

---
+++ contacts.js
@@ -0,0 +1,57 @@
+const fs = require("fs").promises;
+const uniqid = require("uniqid");
+const path = require("path");
+
+const listContactsPath = path.resolve("db/contacts.json");
+async function updateDataContacts(newDataContacts) {
+  await fs.writeFile(listContactsPath, JSON.stringify(newDataContacts));
+}
+
+async function getlistContacts() {
+  const data = await fs.readFile(listContactsPath, "utf8");
+  const contacts = JSON.parse(data);
+  return contacts;
+}
+
+async function getContactById(contactId) {
+  const allContacts = await getlistContacts();
+  const contactById = allContacts.find((contact) => contact.id === contactId);
+
+  if (!contactById) {
+    return null;
+  }
+
+  return contactById;
+}
+
+async function removeContact(contactId) {
+  const allContacts = await getlistContacts();
+  const idx = allContacts.findIndex((item) => item.id === contactId);
+
+  if (idx === -1) {
+    return null;
+  }
+
+  const [newLiscContacts] = allContacts.splice(idx, 1);
+  await updateDataContacts(allContacts);
+
+  return newLiscContacts;
+}
+
+async function addContact(name, email, phone) {
+  const allContacts = await getlistContacts();
+
+  const contactAdd = { id: uniqid(), name, email, phone };
+  allContacts.push(contactAdd);
+
+  await updateDataContacts(allContacts);
+
+  return contactAdd;
+}
+
+module.exports = {
+  getlistContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
